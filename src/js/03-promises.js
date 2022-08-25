0ca9f21bf0744c6f94276ae94c73d040d3cef5ee
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-25 14:26:41 +0300

    Изменение подключения стилей и начало зад 3 промисы

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,25 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('.form');
+console.log(formRef);
+formRef.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(evt) {
+  evt.preventdefault();
+
+  let delay = Number(evt.currentTarget.delay.value);
+  console.log(delay);
+  const step = Number(evt.currentTarget.step.value);
+  console.log(step);
+  const amount = Number(evt.currentTarget.amount.value);
+  console.log(amount);
 }
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
