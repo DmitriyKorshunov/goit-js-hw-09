c9d88c607f99fb774515bbeafe4f98226172f2fa
DmitriyKorshunov <dkorshunov77@gmail.com>
2022-08-26 13:14:34 +0300

    Зад 3 промисы есть решение и добавлены стили

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,36 +1,38 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const formRef = document.querySelector('.form');
-
 formRef.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm(evt) {
-  evt.preventdefault();
+  evt.preventDefault();
 
-  let delay = Number(evt.currentTarget.delay.value);  
-  const step = Number(evt.currentTarget.step.value);  
+  let delay = Number(evt.currentTarget.delay.value);
+  const step = Number(evt.currentTarget.step.value);
   const amount = Number(evt.currentTarget.amount.value);
-  
 
-  for (let position = 1; position <= amount; position += 1) {
-    createPromise(position, delay).then(({ position, delay }) => {
-        Notify.success(`Fulfilled promise ${position} in ${delay}ms`)
-    }).catch(({ position, delay }) => {
-        Notify.failure(`Rejected promise ${position} in ${delay}ms`)
-    })
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+        }, delay);
+      });
     delay += step;
-    };
-  
+  }
+}
 
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
 
-//   const createPromise = (position, delay) => {
-//   return new Promise((resolve, reject) => {
-//     const shouldResolve = Math.random() > 0.3;
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         resolve({ position, delay });
-//       }
-//       reject({ position, delay });
-//     }, delay);
-//   });
-// 
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    }
+    reject({ position, delay });
+  });
+}
